2bb5493137bb27e5c45e3c7bee86e6bbf7f84069
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2020-04-29 13:44:00 -0300

    dia 29

--- semana3/condicionais/index.js
+++ semana3/condicionais/index.js
@@ -1,93 +1,55 @@
-// ======================= 1 ==========================
+// ============================ Exercício 1 =====================
 
-// Verifica se o número é par ou ímpar
+// Imprime a soma dos números de 1 a 15 que é 115
 
-// ========================= 2 =====================
+// ============================ Exercício 2 =====================
 
-/**
- * (A) Para retornar o preço das frutas
- * (B) O preço da fruta Maça é  R$ 2.25
- * (C) 24.25 reais
- * (D) O preço da fruta pera é  R$ 5.5
- */
+// a) coloca um item no final da nova lista
+// b) [10, 15, 25, 30]
+// c) [12, 15, 18, 21, 27, 30] e [12]
 
-// ====================== 3 ======================
+// ============================ Exercício 3 =====================
 
-// mensagem is not defined
+// A)
 
+let numeros = [10, 30, 20, 40, 15]
 
-// ==================  Exercício 4 ======================
-
-// A
-let numero1 = prompt("Digite um número")
-let numero2 = prompt("Digite outro número")
-
-if (numero1 > numero2){
-    console.log(numero1, numero2)
-}
-else{
-    console.log(numero2, numero1)
-}
-
-// B
-
-let numero1 = Number(prompt("Digite um número"))
-let numero2 = Number(prompt("Digite outro número"))
-let numero3 = Number(prompt("Digite mais outro número"))
-
-let numeros = [numero1, numero2, numero3]
-numeros.sort(function(a, b) {
-    return a - b
-})
-
-console.log(numeros[2])
-console.log(numeros[1])
-console.log(numeros[0])
-
-// C
-
-let numero1 = Number(prompt("Digite um número"))
-let numero2 = Number(prompt("Digite outro número"))
-let numero3 = Number(prompt("Digite mais outro número"))
-
-let numeros = [numero1, numero2, numero3]
-numeros.sort(function(a, b) {
-    return a - b
-})
-
-if (numero1 === numero2 === numero3){
-    console.log("Digite três números diferentes")
-}
-
-console.log(numeros[2])
-console.log(numeros[1])
-console.log(numeros[0])
-
-
-//=============================  5 ===================================
-
-// Link imagem https://drive.google.com/file/d/1fetcHLD40mvaoXymWUoIocpEjcT5qsWZ/view?usp=sharing
-
-let ossos = prompt("Possui ossos")
-let pelos = prompt("Possui pelos")
-let racional = prompt("Racional?")
-let terrestre = prompt("Terrestre?")
-
-if(ossos == true){
-    if(pelos == true){
-        if(racional == true){
-            console.log("Humano")
-        }
-        else{
-            if(terrestre == true){
-                console.log("Animal Terrestre")
-            }
-            else{
-                console.log("Peixe")
-            }
-        }
+let maior = numeros[0]
+let menor = numeros[0]
+for (const numero of numeros) {
+    if(numero > maior){
+        maior = numero
+    }
+    if(numero < menor){
+        menor = numero
     }
 }
-else{
-    console.log("Só queremos classificar animais com ossos")
+console.log(maior)
+console.log(menor)
+
+// B)
+
+let novoArray = []
+for (const numero of numeros){
+    novoArray.push(numero/10)
+}
+
+console.log(novoArray)
+
+// C)
+
+let pares = []
+for (const numero of numeros){
+    if (numero % 2 == 0){
+        pares.push(numero)
+    }
+}
+
+console.log(pares)
+
+// D
+
+for (let i = 0; i < numeros.length; i++) {
+    let mensagem = "O elemento de índice " + i + " é " + numeros[i]
+    console.log(mensagem)
 }

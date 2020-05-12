c0819bb3e0bde5687914403c4a345c1c463f3572
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2020-05-12 14:44:47 -0300

    dia 12 de maio

--- semana5/lebedin/src/App.js
+++ semana5/lebedin/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
 import ImagemButton from './components/ImagemButton/ImagemButton';
+import perfil from './images/perfil.jpg'
+import CardPequeno from './components/CardPequeno/CardPequeno';
+
 
 function App() {
   return (
@@ -9,30 +12,31 @@ function App() {
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
         <CardGrande 
-          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
-          nome="Astrodev" 
-          descricao="Oi, eu sou o Astrodev. Sou o chefe dos alunos da Labenu. Adoro pedir e-mails na sexta-feira e esperar os alunos responderem só para responder com uma bronca e dar mais trabalho para eles."
+          imagem={perfil} 
+          nome="Henrique" 
+          descricao="Oi, eu sou o Henrique."
         />
         
         <ImagemButton 
           imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
           texto="Ver mais"
         />
+
+        <CardPequeno 
+          email={"meuemai@gmail.com"}
+          endereco={"Rua: minha rua"}
+        />
+
       </div>
 
       <div className="page-section-container">
         <h2>Experiências profissionais</h2>
         <CardGrande 
           imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
-          nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+          nome="Empresa" 
+          descricao="Nenhuma" 
         />
         
-        <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
-        />
       </div>
 
       <div className="page-section-container">

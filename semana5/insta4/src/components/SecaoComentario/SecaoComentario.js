45bcb76f09e0d6fba726079c20c62552f4a300c0
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2020-05-13 16:35:09 -0300

    dia 13 de maio

---
+++ semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import './SecaoComentario.css'
+
+export class SecaoComentario extends Component {
+	state = {
+		comentario: 'a'
+	}
+
+	onChangeComentario(event) {
+		this.setState({
+			comentario: "teste"
+		})
+	}
+
+	render() {
+		return <div className={'comment-container'}>
+			<input
+				className={'input-comentario'}
+				placeholder={'Comentário'}
+				value={this.state.comentario}
+				onChange={this.onChangeComentario}
+			/>
+			<button onClick={this.props.aoEnviar}>Enviar</button>
+		</div>
+	}
+}

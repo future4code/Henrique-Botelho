27587f555f3f1b8cb01488fd6824ab4287c6d02f
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2020-07-27 15:18:09 -0300

    dia 27 de julho

---
+++ aula44/ex1.js
@@ -0,0 +1,8 @@
+// Type 2 is no assignable to string
+// var minhaString: string = 2
+var objeto = {
+    nome: "Henrique",
+    idade: 24,
+    corFavorita: "azul"
+};
+console.log(objeto);
---
+++ aula44/ex4.ts
@@ -0,0 +1,6 @@
+// a) usaria o comando tsc
+
+// b) usaria o comando src/tsc
+
+// c) tsc arquivo1.ts arquivo2.ts arquivo3.ts
+

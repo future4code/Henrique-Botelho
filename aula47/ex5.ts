aafcfa9ffba58eb5390ad62340a91cc4f6b75b41
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2020-07-30 16:12:18 -0300

    dia 30 de julho

---
+++ aula47/ex5.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const baseUrl = "https://us-central1-labenu-apis.cloudfunctions.net/labenews/";
+
+type User = {
+	id: string;
+	name: string;
+	email: string;
+}
+
+const sendNotifications = async (
+    users: User[],
+    message: string
+  ): Promise<void> => {
+    const promiseArray: Promise<any>[] = [];
+    for (const user of users) {
+      await axios.post(`${baseUrl}/notifications/send`, {
+        subscriberId: user.id,
+        message: message,
+      });
+    }
+  
+    console.log("All notifications sent");
+  };

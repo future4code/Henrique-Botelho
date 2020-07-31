2ee932376f9565ad4236d1be764c016445a8a23f
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2020-07-31 16:14:38 -0300

    dia 31 de julho

---
+++ aula48/src/criar.ts
@@ -0,0 +1,16 @@
+type conta = {
+    nomeDaConta: string
+}
+
+type transacoes = {
+    nomeDaTransacao: string
+}
+
+
+function createAccount(nome: string, cpf: number, data: number){
+    
+} 
+
+function getAllAccounts(){
+
+}
